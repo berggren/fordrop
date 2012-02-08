2c84deb784e8e192a561bf4c6df827a3828a6120
Johan Berggren <jbn@nordu.net>
2012-02-08 19:26:09 +0100

    Now displays Nodename and nodetite

--- src/fordrop-client.py
+++ src/fordrop-client.py
@@ -14,6 +14,10 @@ def post(uri, data, headers):
 	r = requests.post(options.url + uri, json.dumps(data), headers=headers)
 	return r.status_code
 
+def delete(uri, headers):
+    r = requests.delete(options.url + uri, headers=headers)
+    return r.status_code
+
 if __name__ == '__main__':
     CONFIG_FILENAME = 'fordrop.cfg'
     config = ConfigParser.ConfigParser()
@@ -23,6 +27,9 @@ if __name__ == '__main__':
     parser.add_option('-k', '--api-key', action='store', dest='api-key', default=config.get("fordrop-client", "api-key"), help='api-key for fordrop-server')
     parser.add_option('--url', action='store', dest='url', default=config.get("fordrop-client", "base-url"), help='Base URL for fordrop-server')
     parser.add_option('-n', '--node', action='store', dest='node', help='Show configuration for node')
+    parser.add_option('-t', '--title', action='store', dest='title', help='Title for node when creating')
+    parser.add_option('-c', '--create', action='store_true', dest='create', help='Create new node')
+    parser.add_option('-d', '--delete', action='store_true', dest='delete', help='Delete node, affiliation or subscription')
     parser.add_option('-q', '--query', action='store_true', dest='query', help='Use together with --node NODE to show configuration')
     parser.add_option('-H', '--human-readable', action='store_true', dest='human', help='Show title for node instead of actual name')
     parser.add_option('-a', '--affiliations', action='store_true', dest='affiliations', help='List affiliations for NODE')
@@ -38,13 +45,23 @@ if __name__ == '__main__':
         'X-Fordrop-Username': options.username,
         'X-Fordrop-Api-Key': '123456'
     }
+
     try:
+        # Create node
+        if options.create and options.title:
+            data = {'title': options.title}
+            post('/node/', data, headers=headers)
+
+        # Delete node
+        if options.delete and options.node and not options.affiliations:
+            delete('/node/%s' % options.node, headers=headers)
+
         # List nodes
         if options.query and not options.node:
             nodes = get('/node', headers=headers)['nodes']
             for node in nodes:
                 if options.human:
-                    print node['title']
+                    print node['title'], '(%s)' % node['name']
                 else:
                     print node['name']
 
@@ -58,10 +75,13 @@ if __name__ == '__main__':
             for user in affiliations:
                 print user
 
-        # Add jid as affiliate for node
+        # Add or delete jid as affiliate for node
         if options.affiliations and options.node and options.jid:
-            data = {'affiliation': [[options.jid, 'member']]}
-            post('/affiliations/%s' % options.node, data, headers=headers)
+            if options.delete:
+                delete('/affiliations/%s/%s' % (options.node, options.jid), headers=headers)
+            else:
+                data = {'affiliation': [[options.jid, 'member']]}
+                post('/affiliations/%s' % options.node, data, headers=headers)
 
         # Subscribers on node
         if options.subscribers and options.node:

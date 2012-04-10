0817d42da0e5875376e2ba15038fbda6e5808ee7
Johan Berggren <jbn@nordu.net>
2012-04-10 17:57:24 +0200

    Removed some legacy and added cherrypy as a dependency

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+config = {
+    'description': 'fordrop - tool for a social CERT',
+    'author': 'Johan Berggren',
+    'url': 'https://fordrop.org',
+    'download_url': 'https://fordrop.org',
+    'author_email': 'jbn@nordu.net',
+    'version': '0.1',
+    'install_requires': ['requests', 'sleekxmpp', 'cherrypy'],
+    'packages': ['fordrop', 'fordrop.plugins'],
+    'scripts': ['bin/fordrop', 'bin/fordropd', 'bin/fordrop-rest-server'],
+    'name': 'fordrop'
+}
+
+setup(**config)

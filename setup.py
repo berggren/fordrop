fc134ba7f2b2c51eee5e25457200cf72e564388a
Johan Berggren <jbn@nordu.net>
2012-04-11 10:58:45 +0200

    Updated to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'url': 'https://fordrop.org',
     'download_url': 'https://fordrop.org',
     'author_email': 'jbn@nordu.net',
-    'version': '0.1',
+    'version': '0.1.1',
     'install_requires': ['requests', 'sleekxmpp', 'cherrypy'],
     'packages': ['fordrop', 'fordrop.plugins'],
     'scripts': ['bin/fordrop', 'bin/fordropd', 'bin/fordrop-rest-server'],

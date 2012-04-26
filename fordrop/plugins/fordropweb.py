5b2067a6541764168f02959584f2e52442135f23
Johan Berggren <jbn@nordu.net>
2012-04-26 11:41:55 +0200

    Bugfix

--- fordrop/plugins/fordropweb.py
+++ fordrop/plugins/fordropweb.py
@@ -4,37 +4,14 @@ import requests
 baseurl = 'http://127.0.0.1:8080/api/v1/'
 headers = {'content-type': 'application/json'}
 
-activity = json.dumps({
-    "published": "2012-04-25T08:51:38.031593+00:00",
-    "verb": "post",
-    "actor": {
-        "web": "http://jbn.klutt.se/",
-        "displayName": "Johan Berggren",
-        "bio": "Foobar",
-        "location": "Sweden",
-        "id": "urn:uuid:5ad9cf86-deb5-4ea9-982d469b",
-        "objectType": "person"
-    },
-    "object": {
-        "objectType": "fordropFile",
-        "hash": {
-            "sha256": "bbc716c8b3df5cc31b52160ddb4e7c2efde37ec0713f56c6193604be8a642627",
-            "sha1": "44cdcbea81e3bc6af2450c84568559b605da324f",
-            "sha512": "1d06b333ffb01d6cda0b66217d95c074ab6a66a6455dae0b23ec26dc6df2397a67a04a0356972f7dbaae70140471bac4f0c7ada111e38e0a29414824109e0e4e",
-            "md5": "7307472f473a58602cdf14b586389b21"
-        },
-        "id": "urn:uuid:a622f8f4-0a39-a29",
-        "description": ""
-    }
-})
-
 def get_or_create_file(activity):
     activity_json = json.loads(activity)
     file = activity_json.get('object')
     hashes = file.get('hash')
     if not file.get('objectType') == 'fordropFile':
         return
-    user = get_or_create_user(activity)
+    profile = get_or_create_user(activity)
+    user = profile.get('user')
     if not user:
         return
     def check_if_file_exist(file):
@@ -74,7 +51,7 @@ def get_or_create_user(activity):
     user_exists = check_if_user_exist(actor)
     if user_exists:
         return json.loads(user_exists.content).get('objects')[0]
-    user_payload = {'username': actor.get('id'), 'is_active': False}
+    user_payload = {'username': actor.get('id'), 'is_active': True}
     profile_payload = json.dumps({
                 'user': user_payload,
                 'name': actor.get('displayName', ''),
